f8e1d1a86b7b9a35a2cf7f20a158805a24959393
ViViNika <89837082+xlm1226@users.noreply.github.com>
2022-02-22 01:14:47 +0800

    Create Snippet18_DistSQL_RAL_Syntax_Chapter06.sql

---
+++ Code_Snippets_Chapter06/Snippet18_DistSQL_RAL_Syntax_Chapter06.sql
@@ -0,0 +1,5 @@
+/** 1. Enable/disable Read Databases**/
+[enable / disable] readwrite_splitting read xxx [from schema]
+
+/** 2. Query All Read Databases **/
+show readwrite_splitting read resources [from schema]
