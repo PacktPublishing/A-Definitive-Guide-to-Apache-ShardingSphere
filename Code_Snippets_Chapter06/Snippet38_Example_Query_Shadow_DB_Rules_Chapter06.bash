33febf9b4c66e542e6aa925f12c3a45d87448378
ViViNika <89837082+xlm1226@users.noreply.github.com>
2022-02-22 09:55:39 +0800

    Create Snippet38_Example_Query_Shadow_DB_Rules_Chapter06.bash

---
+++ Code_Snippets_Chapter06/Snippet38_Example_Query_Shadow_DB_Rules_Chapter06.bash
@@ -0,0 +1,8 @@
+mysql> show shadow rule shadow_rule_1;
+
++------------------+-------------+-------------+--------------+
+| rule_name        | source_name | shadow_name | shadow_table |
++------------------+-------------+-------------+--------------+
+| shadow_rule_1    | ds_1        | ds_shadow_1 | t_order      |
++------------------+-------------+-------------+--------------+
+1 rows in set (0.01 sec)
