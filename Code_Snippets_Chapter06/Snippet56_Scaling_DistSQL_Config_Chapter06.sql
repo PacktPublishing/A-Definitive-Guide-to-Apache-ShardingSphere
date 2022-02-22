fd78698697a28f9ad087f867d2548694bf5a4fd7
ViViNika <89837082+xlm1226@users.noreply.github.com>
2022-02-22 10:26:06 +0800

    Create Snippet56_Scaling_DistSQL_Config_Chapter06.sql

---
+++ Code_Snippets_Chapter06/Snippet56_Scaling_DistSQL_Config_Chapter06.sql
@@ -0,0 +1,11 @@
+createShardingScaling
+    : CREATE SHARDING SCALING scalingName
+    ;
+
+dropShardingScaling
+    : DROP SHARDING SCALING scalingName
+    ;
+
+scalingName
+    : IDENTIFIER
+    ;
