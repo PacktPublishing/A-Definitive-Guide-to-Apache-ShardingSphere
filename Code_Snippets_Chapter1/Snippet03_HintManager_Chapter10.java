d853a0b2d926123f0be66aee5304c179869ba1a6
ViViNika <89837082+xlm1226@users.noreply.github.com>
2022-03-15 12:33:45 +0800

    Create Snippet03_HintManager_Chapter10.java

---
+++ Code_Snippets_Chapter1/Snippet03_HintManager_Chapter10.java
@@ -0,0 +1,15 @@
+String sql = "SELECT * FROM t_order";
+// Obatin HintManager Instance
+try (HintManager hintManager = HintManager.getInstance();
+     Connection connection = dataSource.getConnection();
+     PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+    // Add Database Sharding Key
+    hintManager.addDatabaseShardingValue("t_order", 2);
+    // Add Table Sharding Value
+    hintManager.addTableShardingValue("t_order", 1);
+    try (ResultSet resultSet = preparedStatement.executeQuery()) {
+        while (resultSet.next()) {
+            // ...
+        }
+    }
+}
